7a75d52a24b812c64333247a11d78ca3c972d5a2
Brandon Kim <bkim2490@gmail.com>
2018-05-29 17:18:09 +0900

    error with RoomList.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAxMxszluxkQnE6ccl1LwhmrG0laQYQxYs",
+  authDomain: "bloc-chat-d32ae.firebaseapp.com",
+  databaseURL: "https://bloc-chat-d32ae.firebaseio.com",
+  projectId: "bloc-chat-d32ae",
+  storageBucket: "bloc-chat-d32ae.appspot.com",
+  messagingSenderId: "170103426822"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <RoomList />
+        </main>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      console.log(snapshot);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default RoomList;
