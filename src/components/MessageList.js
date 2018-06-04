0652f6680354255636026ad81675989d80c94f87
Brandon Kim <bkim2490@gmail.com>
2018-06-04 12:44:20 +0900

    display username and time sent when displaying message

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -18,6 +18,23 @@ class MessageList extends Component {
     this.setState({ newMessage: event.target.value });
   }
 
+  deleteMessage(message) {
+    if(this.props.user !== null && this.props.user.displayName !== message.user) {
+      alert("Need to be room creator");
+      return;
+
+    } else if (this.props.user === null && message.user !== 'Guest') {
+      alert("Need to be room creator");
+      return;
+    }
+    this.props.deleteSingleMessage(message);
+  }
+
+  formatTime(time) {
+    let myDate = new Date(time);
+    return myDate.toDateString() + ' @ ' + myDate.getHours() + ':' + myDate.getMinutes();
+  }
+
   render() {
     return (
       <section className="messageList">
@@ -25,12 +42,19 @@ class MessageList extends Component {
         {
           this.props.activeMessages.map( (message, index) =>
             <div className="message" key={index}>
-              {message.content}
+              <span className="user">{message.user}</span>
+              <span className="time">{this.formatTime(message.sentAt)}</span>
+              <div className="content">{message.content}</div>
+              <input className="deleteButton" type="button" value="Delete" onClick={(e) => this.deleteMessage(message)}></input>
             </div>
           )
         }
         <form className="messageForm" onSubmit={ (e, message) => this.createMessage(e, this.state.newMessage) }>
-          <input className="newMessage" type="text" placeholder="Enter your message" value={this.state.newMessage} onChange={ (e) => this.handleChange(e) }></input>
+          <input className="newMessage"
+            type="text"
+            placeholder="Enter your message"
+            value={this.state.newMessage}
+            onChange={ (e) => this.handleChange(e) }></input>
           <input type="submit" value="Submit"></input>
         </form>
       </section>
